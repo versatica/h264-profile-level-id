c026c7b88e3cd44cfa8f2f84bf6948bc80cdbe9e
Iñaki Baz Castillo <ibc@aliax.net>
2025-07-21 15:57:45 +0200

    improve npm-scripts

--- npm-scripts.mjs
+++ npm-scripts.mjs
@@ -23,12 +23,12 @@ const PRETTIER_PATHS = [
 ].join(' ');
 
 const task = process.argv[2];
-const args = process.argv.slice(3).join(' ');
+const taskArgs = process.argv.slice(3).join(' ');
 
 run();
 
 async function run() {
-	logInfo(args ? `[args:"${args}"]` : '');
+	logInfo(taskArgs ? `[args:"${taskArgs}"]` : '');
 
 	switch (task) {
 		// As per NPM documentation (https://docs.npmjs.com/cli/v9/using-npm/scripts)
@@ -56,7 +56,7 @@ async function run() {
 
 		case 'typescript:watch': {
 			deleteLib();
-			executeCmd(`tsc --watch ${args}`);
+			executeCmd(`tsc --watch ${taskArgs}`);
 
 			break;
 		}
@@ -82,7 +82,7 @@ async function run() {
 
 		case 'coverage': {
 			buildTypescript({ force: false });
-			executeCmd(`jest --coverage ${args}`);
+			executeCmd(`jest --coverage ${taskArgs}`);
 			executeCmd('open-cli coverage/lcov-report/index.html');
 
 			break;
@@ -100,7 +100,7 @@ async function run() {
 			executeCmd(`git tag -a ${PKG.version} -m '${PKG.version}'`);
 			executeCmd(`git push origin ${RELEASE_BRANCH}`);
 			executeCmd(`git push origin '${PKG.version}'`);
-			executeCmd('npm publish');
+			executeInteractiveCmd('npm publish');
 
 			break;
 		}
@@ -157,7 +157,7 @@ function format() {
 function test() {
 	logInfo('test()');
 
-	executeCmd(`jest --silent false --detectOpenHandles ${args}`);
+	executeCmd(`jest --silent false --detectOpenHandles ${taskArgs}`);
 }
 
 function installDeps() {
@@ -198,20 +198,31 @@ function executeCmd(command, exitOnError = true) {
 	}
 }
 
-function logInfo(message) {
-	// eslint-disable-next-line no-console
-	console.log(`npm-scripts \x1b[36m[INFO] [${task}]\x1b[0m`, message);
+function executeInteractiveCmd(command) {
+	logInfo(`executeInteractiveCmd(): ${command}`);
+
+	try {
+		execSync(command, { stdio: 'inherit', env: process.env });
+	} catch (error) {
+		logError(`executeInteractiveCmd() failed, exiting: ${error}`);
+
+		exitWithError();
+	}
 }
 
-// eslint-disable-next-line no-unused-vars
-function logWarn(message) {
+function logInfo(...args) {
 	// eslint-disable-next-line no-console
-	console.warn(`npm-scripts \x1b[33m[WARN] [${task}]\x1b[0m`, message);
+	console.log(`npm-scripts.mjs \x1b[36m[INFO] [${task}]\x1b[0m`, ...args);
 }
 
-function logError(message) {
+function logWarn(...args) {
 	// eslint-disable-next-line no-console
-	console.error(`npm-scripts \x1b[31m[ERROR] [${task}]\x1b[0m`, message);
+	console.warn(`npm-scripts.mjs \x1b[33m[WARN] [${task}]\x1b\0m`, ...args);
+}
+
+function logError(...args) {
+	// eslint-disable-next-line no-console
+	console.error(`npm-scripts.mjs \x1b[31m[ERROR] [${task}]\x1b[0m`, ...args);
 }
 
 function exitWithError() {
